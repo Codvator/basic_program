b60f084570592e84801086d735667d58dd1bd111
Siddharth Singh <siddharthsingh07112001@gmail.com>
2024-11-12 14:13:05 +0530

    Add files via upload

---
+++ code (6).cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a,b;
+	cout<<"Enter the first number "<<endl;
+	cin>>a;
+	cout<<"Enter the second number"<<endl;
+	cin>>b;
+	int sum = a+b;
+	cout<<"The sum of the two numbers are  "<<sum;
+	
+	return 0;
+}
